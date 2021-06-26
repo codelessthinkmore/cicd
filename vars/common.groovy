cd91f30f5b6c545e60a058eacfbde09461ea2179
codelessthinkmore <71873100+codelessthinkmore@users.noreply.github.com>
2021-06-26 20:30:05 +0530

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,4 +1,51 @@
-def firstfunction() 
+def checkoutrepo(url)
 {
- print("this is my first function")
+    sh """
+
+    git clone ${url}
+    
+    """
+
+}
+
+def BuildDockerImage(reponame,imagename,regurl)
+{
+    sh """
+    
+    cd ${reponame}
+    
+    ls -l
+
+    docker build  --tag "$regurl/$imagename:v$BUILD_NUMBER" .
+    
+    docker push "$regurl/$imagename:v$BUILD_NUMBER"
+
+    export GoDockerBuildTag="$BUILD_NUMBER"
+
+    """
+
+}
+
+def getLastSuccessfulBuildNumber(String jobPath){
+	/*
+	params:
+		jobPath: type String :: path of job including folder name.
+						 Ex:  DAaaS-k8s-CI-mergereq2master/CI-INTEGRATION_TEST
+	*/
+	
+	def lastSuccessfulBuildNumber = ""
+	try{
+		if (jobPath.contains('/')){
+			def job = Jenkins.getInstance().getItemByFullName(jobPath, Job.class)
+			lastSuccessfulBuildNumber = job.lastSuccessfulBuild.displayName
+		}else{
+			lastSuccessfulBuildNumber = Jenkins.instance.getItem(jobPath).lastSuccessfulBuild.displayName
+		}
+		
+	}catch(Exception){
+		println(String.format("job: %s is invalid", jobPath))
+		println("exception: "+Exception)
+	}
+	println(String.format("Last Successful Build number of job: %s  is %s", jobPath, lastSuccessfulBuildNumber))
+	return lastSuccessfulBuildNumber.replace("#", "")
 }
